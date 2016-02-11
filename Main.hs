eb6470d38fda8f0ff715d56f833b96e80337dd5b
Greg Kettler <gkettler@alum.mit.edu>
2016-02-10 19:45:37 -0600

    simply to copy from a texture to the renderer... sprite sheet?

--- Main.hs
+++ Main.hs
@@ -2,19 +2,33 @@
 
 module Main where
 
-import Linear (V4(..))
+import Linear (V4(..), V2(..))
 import Control.Monad (unless)
 import SDL
+import Linear.Affine 
+import Foreign.C.Types
+p1 :: Point V2 Foreign.C.Types.CInt
+p1 = P (V2 10 10)
+rect :: Rectangle CInt
+rect = Rectangle p1 (V2 20 20)
 
 main :: IO ()
 main = do
   initializeAll
   window <- createWindow "My SDL Application" defaultWindow
   renderer <- createRenderer window (-1) defaultRenderer
-  appLoop renderer
+  texture <- createTexture renderer 
+                           RGB24 
+                           TextureAccessTarget 
+                           (V2 255 255)
+  rendererDrawColor renderer $= V4 0 0 255 255
+  clear renderer
+  copy renderer texture (Just rect) (Just rect)
+  present renderer
+  appLoop renderer texture
 
-appLoop :: Renderer -> IO ()
-appLoop renderer = do
+appLoop :: Renderer -> Texture -> IO ()
+appLoop renderer texture = do
   events <- pollEvents
   let eventIsQPress event =
         case eventPayload event of
@@ -23,8 +37,5 @@ appLoop renderer = do
             keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
           _ -> False
       qPressed = not (null (filter eventIsQPress events))
-  rendererDrawColor renderer $= V4 0 0 255 255
-  clear renderer
-  present renderer
-  unless qPressed (appLoop renderer)
+  unless qPressed (appLoop renderer texture)
 
