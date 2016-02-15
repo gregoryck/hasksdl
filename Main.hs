346d9466c6b1c1c0632527dbcb2d1d48271f23f3
Greg Kettler <gkettler@alum.mit.edu>
2016-02-15 15:55:56 -0600

    isolating a sprite. Watch the todo-notation

--- Main.hs
+++ Main.hs
@@ -7,25 +7,61 @@ import Control.Monad (unless)
 import SDL
 import Linear.Affine 
 import Foreign.C.Types
+
+
 p1 :: Point V2 Foreign.C.Types.CInt
-p1 = P (V2 10 10)
+p1 = P (V2 291 97)
 rect :: Rectangle CInt
-rect = Rectangle p1 (V2 20 20)
+rect = Rectangle p1 (V2 26 46)
+
+data SizedTex = SizedTex SDL.Texture (V2 CInt)
+
+loadTexture :: SDL.Renderer -> FilePath -> IO SizedTex
+loadTexture r filePath = do
+  surface <- SDL.loadBMP filePath
+  size <- SDL.surfaceDimensions surface
+  let key = V4 255 255 255 255
+  SDL.surfaceColorKey surface $= Just key
+  t <- SDL.createTextureFromSurface r surface
+  SDL.freeSurface surface
+  return $ SizedTex t size
+
+unsized :: SizedTex -> SDL.Texture
+unsized (SizedTex texture _) = texture
 
 main :: IO ()
 main = do
   initializeAll
   window <- createWindow "My SDL Application" defaultWindow
   renderer <- createRenderer window (-1) defaultRenderer
-  texture <- createTexture renderer 
-                           RGB24 
-                           TextureAccessTarget 
-                           (V2 255 255)
+  texture <- loadTexture renderer "npcs.bmp"
   rendererDrawColor renderer $= V4 0 0 255 255
   clear renderer
-  copy renderer texture (Just rect) (Just rect)
+  copy renderer (unsized texture) (Just rect) (Just rect)
   present renderer
-  appLoop renderer texture
+  appLoop renderer (unsized texture)
+
+data Action = Quit | Up | Down | MoveLeft | MoveRight
+
+buttonPressed :: Event -> Maybe Action
+buttonPressed event =
+-- TODO do notation
+        case eventPayload event of
+            KeyboardEvent keyboardEvent ->
+                case keyboardEventKeyMotion keyboardEvent of 
+                    Pressed ->
+                         case keysymKeycode (keyboardEventKeysym keyboardEvent) of
+                             KeycodeQ -> Just Quit
+                             KeycodeW -> Just Up
+                             KeycodeS -> Just Down
+                             KeycodeA -> Just MoveLeft 
+                             KeycodeD -> Just MoveRight
+                             _        -> Nothing
+
+                    _ -> Nothing
+            _ -> Nothing
+
+
 
 appLoop :: Renderer -> Texture -> IO ()
 appLoop renderer texture = do
